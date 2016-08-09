7e33307331e933d39df32dcdb05b7770462339a1
Feliciano, Matthew <Matthew.Feliciano@cdk.com>
2016-08-09 12:16:55 -0700

    Testing peer code

--- Peer_Review_Assignment07-2.py
+++ Peer_Review_Assignment07-2.py
@@ -1,21 +1,20 @@
 """-------------------------------------------------------------Title: Python Pickling ProgramDev: Dan PageDate: 08/07/2016Description: This Program takes in the user's favorite animal andthe sounds they make and stores it in a list.  It then saves toa .dat file and reads the loads the list back from the .dat file--------------------------------------------------------------"""# Program Start
 import pickle
-animal = ''
-objFile = None
-# Store Animal and their sound in a
-listanimal = str(input("Enter a your favorite animal: "))
+
+# Store Animal and their sound in a list
+animal = str(input("Enter a your favorite animal: "))
 animal_sound = str(input("What sound does your favorite animal make? "))
 animal_lst = [animal, animal_sound]
 
 print("From the User: ", animal_lst)
 
-# Store Data with Pickle
-MethodobjFile = open("AnimalSounds.dat", "wb")
+# Store Data with Pickle Method
+objFile = open("AnimalSounds.dat", "wb")
 pickle.dump(animal_lst, objFile)
 objFile.close()
 
-# Read Data with Pickle
-MethodobjFile = open("AnimalSounds.dat", "rb")
+# Read Data with PickleMethod
+objFile = open("AnimalSounds.dat", "rb")
 objFileData = pickle.load(objFile)
 objFile.close()
 
